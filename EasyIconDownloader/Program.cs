96958eaac9c6d44774cb00fa9a5b99584a33383d
lvxl <xl.lv@fugro.com>
2021-04-16 08:58:56 +0800

    使用配置文件配置API路径、输出路径。

--- EasyIconDownloader/Program.cs
+++ EasyIconDownloader/Program.cs
@@ -9,14 +9,6 @@ namespace EasyIconDownloader
 {
     class Program
     {
-        private const string APIURL = "https://www.easyicon.net/api/resizeApi.php?id={id}&size={size}";
-
-        // private static List<int> PixeList = new List<int>()
-        // {
-        //     16, 24, 32, 48, 64, 72, 96, 128, 256, 512
-        // };
-
-
         static void Main(string[] args)
         {
             while (true)
@@ -32,7 +24,7 @@ namespace EasyIconDownloader
             var b = int.TryParse(Console.ReadLine(), out int id);
             if (!b)
             {
-                Console.WriteLine("Not a Id.");
+                Console.WriteLine("Not an Id.");
                 return;
             }
 
@@ -40,16 +32,20 @@ namespace EasyIconDownloader
             var fileName = Console.ReadLine();
             
             List<int> PixeList = ConfigurationHelper.GetValue<List<int>>("PixeList");
-            
-            var dir = $"d:\\EasyIcon\\{id}_{fileName}\\";
-            if (!Directory.Exists(dir))
-                Directory.CreateDirectory(dir);
+
+            var outputDir = ConfigurationHelper.GetValue<string>("OutputDir");
+            outputDir = Path.Combine(outputDir, $"{id}_{fileName}");
+            if (!Directory.Exists(outputDir))
+                Directory.CreateDirectory(outputDir);
+
             foreach (int pxInt in PixeList)
             {
-                var apiUrl = APIURL.Replace("{id}", id.ToString()).Replace("{size}", pxInt.ToString());
+                var apiUrl = ConfigurationHelper.GetValue<string>("APIURL");
+                apiUrl = apiUrl.Replace("{id}", id.ToString()).Replace("{size}", pxInt.ToString());
                 var web = new WebClient();
                 var pngBytes = web.DownloadData(new Uri(apiUrl));
-                File.WriteAllBytes(Path.Combine(dir, $"{fileName}_{pxInt}px.png"), pngBytes);
+                if (pngBytes.Length > 100)
+                    File.WriteAllBytes(Path.Combine(outputDir, $"{fileName}_{pxInt}px.png"), pngBytes);
             }
         }
     }
